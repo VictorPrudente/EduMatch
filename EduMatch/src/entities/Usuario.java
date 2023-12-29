83dee0b92a53752ff106acfda61e8dc7066fcdad
Prudente <victorprudt@gmail.com>
2023-12-29 01:09:50 -0300

    Criação da classe Usuário

--- EduMatch/src/entities/Usuario.java
+++ EduMatch/src/entities/Usuario.java
@@ -1,4 +1,21 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
+
+    String login;
+    String senha;
+    Integer tipoDeUser;
+    String CPF;
+    Boolean AutorizacaoDosPais;
+    Integer idade;
+    Endereco endereco;
+    Integer pontuacao;
+    
+    public void aumentarPontuacao(int pontos){
+        this.pontuacao += pontos;
+    }
+
 }
