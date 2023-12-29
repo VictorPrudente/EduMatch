ee4d677a7fc2c4eaf66d2441a54feb1b31c44b40
Debora Hickmann <deboraaahs@gmail.com>
2023-12-29 05:57:09 -0300

    Adicionando classe escola

---
+++ EduMatch/src/entities/Escola.java
@@ -0,0 +1,11 @@
+package EduMatch.src.entities;
+
+public class Escola {
+
+        String nome;
+        int tipo;
+        String cnpj;
+        public void cadastrarAluno (String nomeAluno){
+            System.out.println("Realizado o cadastro do aluno: " + nomeAluno);
+        }
+}
