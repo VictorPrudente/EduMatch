16549519b91dcb1672bdfc3947725352fd6575ed
Carlos Alberto Estrela <carlos.alberto-estela@hotmail.com>
2024-01-06 16:03:21 -0300

    add: interface Service

---
+++ EduMatch/src/interfaces/Service.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public interface Service {
+
+    public boolean salvar();
+    public boolean atualizar();
+    public boolean deletar();
+    public void listarTodos();
+
+}
