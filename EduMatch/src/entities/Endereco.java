84b1d6e71998a0186ec7a4899834cc8535daf96f
Victor Prudente <143949574+VictorPrudente@users.noreply.github.com>
2024-01-07 15:18:06 -0300

    Revert "Feat/menu"

--- EduMatch/src/entities/Endereco.java
+++ EduMatch/src/entities/Endereco.java
@@ -1,113 +1,20 @@
 package entities;
 
 public class Endereco {
-    private int id;
-    private String logradouro;
-    private Integer numero;
-    private String complemento;
-    private String cep;
-    private String cidade;
-    private String estado;
-    private String pais;
+    String logradouro;
+    Integer numero;
+    String complemento;
+    String cep;
+    String cidade;
+    String estado;
+    String pais;
 
-    public Endereco(){}
-
-    public Endereco(int id,String logradouro, Integer numero, String complemento, String cep, String cidade, String estado, String pais){
-        this.id = id;
-        this.logradouro = logradouro;
-        this.numero = numero;
-        this.complemento = complemento;
-        this.cep = cep;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.pais = pais;
-    }
-
-    public Endereco(String logradouro, Integer numero, String complemento, String cep, String cidade, String estado, String pais){
-        this.logradouro = logradouro;
-        this.numero = numero;
-        this.complemento = complemento;
-        this.cep = cep;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.pais = pais;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("""
-                -=-=-=-=-=-=-=-=-=-=-=
-                ID: %d
-                Logradouro: %s
-                Número: %d
-                Complemento: %s
-                CEP: %s
-                Cidade: %s
-                Estado: %s
-                País: %s""", id, logradouro, numero, complemento, cep, cidade, estado, pais);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLogradouro(){
-        return logradouro;
-    }
-
-    public void setLogradouro(String logradouro){
-        this.logradouro = logradouro;
-    }
-
-    public Integer getNumero(){
-        return numero;
-    }
-
-    public void setNumero(Integer numero){
-        this.numero = numero;
-    }
-
-    public String getComplemento(){
-        return complemento;
-    }
-
-    public void setComplemento(String complemento){
-        this.complemento = complemento;
-    }
-
-    public String getCep(){
-        return cep;
-    }
-
-    public void setCep(String cep){
-        this.cep = cep;
-    }
-
-    public String getCidade(){
-        return cidade;
-    }
-
-    public void setCidade(String cidade){
-        this.cidade = cidade;
-    }
-
-    public String getEstado(){
-        return estado;
-    }
-
-    public void setEstado(String estado){
-        this.estado = estado;
-    }
-
-    public String getPais(){
-        return pais;
-    }
-
-    public void setPais(String pais){
-        this.pais = pais;
+    public void imprimirEndereco(){
+        System.out.println("Endereço: " + this.logradouro + "," + this.numero);
+        System.out.println("Complemento: " + this.complemento);
+        System.out.println("CEP: " + this.cep);
+        System.out.println("Cidade" + this.cidade);
+        System.out.println("Estado: " + this.estado);
+        System.out.println("País: " + this.pais);
     }
 }
--- EduMatch/src/entities/Usuario.java
+++ EduMatch/src/entities/Usuario.java
@@ -4,64 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-
 public class Usuario {
-
-    private Integer id;
     private String nome;
     private String sobrenome;
+    private String email;
+    private String login;
+    private String senha;
+    private Integer tipoDeUser;
     private String CPF;
     private Integer idade;
+    private Endereco endereco;
     private Integer pontuacao;
-    private List<Endereco> enderecos = new ArrayList<>();
-    private List<Contato> contatos = new ArrayList<>();
-    private Escola escola;
+
     public Usuario() {
     }
 
-    public Usuario(String nome, String sobrenome, String CPF,
-                   Integer idade, Integer pontuacao) {
+    public Usuario(String nome, String sobrenome, String login, String senha, Integer tipoDeUser, String CPF,
+                   Integer idade, Endereco endereco, Integer pontuacao) {
         this.nome = nome;
         this.sobrenome = sobrenome;
+        this.login = login;
+        this.senha = senha;
+        this.tipoDeUser = tipoDeUser;
         this.CPF = CPF;
         this.idade = idade;
+        this.endereco = endereco;
         this.pontuacao = pontuacao;
+    }
+
+    public void aumentarPontuacao(int pontos){
+        this.pontuacao += pontos;
     }
 
     public boolean temAutorizacaoDosPais(){
         return false;
-    }
-
-    public Escola getEscola() {
-        return escola;
-    }
-
-    public void setEscola(Escola escola) {
-        this.escola = escola;
-    }
-
-    public List<Endereco> getEnderecos() {
-        return enderecos;
-    }
-
-    public void addEnderecos(Endereco endereco) {
-        this.enderecos.add(endereco);
-    }
-
-    public List<Contato> getContatos() {
-        return contatos;
-    }
-
-    public void addContatos(Contato contato) {
-        this.contatos.add(contato);
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id){
-        this.id = id;
     }
 
     public String getNome() {
@@ -80,6 +56,30 @@ public class Usuario {
         this.sobrenome = sobrenome;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Integer getTipoDeUser() {
+        return tipoDeUser;
+    }
+
+    public void setTipoDeUser(Integer tipoDeUser) {
+        this.tipoDeUser = tipoDeUser;
+    }
+
     public String getCPF() {
         return CPF;
     }
@@ -96,19 +96,39 @@ public class Usuario {
         this.idade = idade;
     }
 
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
     public Integer getPontuacao() {
         return pontuacao;
     }
 
-    public void setPontuacao(Integer pontuacao){
-        this.pontuacao += pontuacao;
+    public String getEmail() {
+        return email;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
-        return String.format("""
-                Nome: %s %s
-                Idade: %d
-                Pontucação: %d""", nome, sobrenome, idade, pontuacao);
+        return
+                "nome= " + nome + '\'' +
+                ", sobrenome= " + sobrenome + '\'' +
+                ", email= " + email + '\'' +
+                ", login='" + login + '\'' +
+                ", senha='" + senha + '\'' +
+                ", tipoDeUser=" + tipoDeUser +
+                ", CPF='" + CPF + '\'' +
+                ", idade=" + idade +
+                ", endereco=" + endereco +
+                ", pontuacao=" + pontuacao;
     }
 
     @Override
@@ -116,12 +136,11 @@ public class Usuario {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Usuario usuario = (Usuario) o;
-        return Objects.equals(id, usuario.id);
+        return Objects.equals(CPF, usuario.CPF);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(CPF);
     }
-
 }
