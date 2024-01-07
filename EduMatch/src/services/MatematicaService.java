367c7f2df39da8e247190e6a1b427aad3da4bbba
Carlos Alberto Estrela <carlos.alberto-estela@hotmail.com>
2024-01-07 16:04:03 -0300

    fix:Correção de perguntas apagadas na classe MatemáticaService

--- EduMatch/src/services/MatematicaService.java
+++ EduMatch/src/services/MatematicaService.java
@@ -40,7 +40,7 @@ public class MatematicaService implements Service<Matematica> {
                 B) 34
                 C) 42
                 D) 37
-                E) 44""","A",1));
+                E) 44""","E",1));
 
         perguntas.add(new Matematica(COUNTER.incrementAndGet(), Dificuldades.FACIL, """
                 
