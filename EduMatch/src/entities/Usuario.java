0a40303209060bbb7b8a8bd5f8090f3c8a700235
Carlos Alberto Estrela <carlos.alberto-estela@hotmail.com>
2024-01-08 11:02:44 -0300

    Fix: correção de bug do git que não permitia o merge com a branch Develop

--- EduMatch/src/entities/Usuario.java
+++ EduMatch/src/entities/Usuario.java
@@ -15,7 +15,9 @@ public class Usuario {
     private Integer pontuacao;
     private List<Endereco> enderecos = new ArrayList<>();
     private List<Contato> contatos = new ArrayList<>();
+    private List<Certificado> certificados = new ArrayList<>();
     private Escola escola;
+
     public Usuario() {
     }
 
