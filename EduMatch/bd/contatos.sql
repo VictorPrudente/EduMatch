4168ba5b8b8e1315ba6c9f5f9f26561d916349b5
Camila Bauer <camila.bauer@gmail.com>
2024-01-12 11:34:21 -0300

    Inicio Inserts contatos

---
+++ EduMatch/bd/contatos.sql
@@ -0,0 +1,98 @@
+--INSERT CONTATO EMPRESA
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,1,'Administrativo','513262-1298', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,2,'Telefone Fixo','216262-1245', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,3,'SAC','544262-3288', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,4,'Telefone Fixo','513626-1245', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,5,'Telefone Fixo','513232-1245', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,6,'Comercial','118678-1144', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,7,'WhatsApp','778362-9955', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,8,'Telefone Fixo','776762-1200', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,9,'Telefone Fixo','776262-1245', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_empresa, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,10,'WhatsApp','516622-1245', 'COMERCIAL');
+
+
+
+--INSERT CONTATO USUARIO
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,1,'Casa','513262-1212', 'RESIDENCIAL');
+
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,2,'WhatsApp','219956-2343', 'CELULAR');
+
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,3,'WhatsApp','549856-2856', 'CELULAR');
+
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,4,'WhatsApp','5198167-9609', 'CELULAR');
+
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,5,'Casa','516856-3576', 'RESIDENCIAL');
+
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,6,'Celular','119856-3211', 'CELULAR');
+
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,7,'WhatsApp','779877-2254', 'CELULAR');
+
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,8,'Casa','779856-2856', 'RESIDENCIAL');
+
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,9,'WhatsApp','779347-0042', 'CELULAR');
+
+INSERT INTO CONTATO (id_contato, id_usuario, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,10,'WhatsApp','5198290-9077', 'CELULAR');
+
+
+
+--INSERT CONTATO ESCOLA
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,1,'WhatsApp','516323-1245', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,2,'WhatsApp','215433-1122', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,3,'Telefone Fixo','546333-1255', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,4,'Telefone Fixo','516211-1100', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,5,'Telefone Fixo','515433-1988', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,6,'Telefone Fixo','115433-1322', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,7,'Telefone Fixo','775321-7655', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,8,'Telefone Fixo','773633-0000', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,9,'whatsApp','779933-1955', 'COMERCIAL');
+
+INSERT INTO CONTATO (id_contato, id_escola, descricao, telefone, tipo_contato)
+VALUES (SEQ_CONTATO.NEXTVAL,10,'Telefone Fixo','516733-0023', 'COMERCIAL');
