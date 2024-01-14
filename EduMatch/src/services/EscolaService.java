91fd86c4a1f9cb6cf29c8964fb8f2179ac280b11
brayanbenet <gabrielbrayan510@gmail.com>
2024-01-14 01:08:15 -0300

    Refacture: EscolaService

--- EduMatch/src/services/EscolaService.java
+++ EduMatch/src/services/EscolaService.java
@@ -1,66 +1,77 @@
 package services;
 
+import entities.Endereco;
 import entities.Escola;
 import entities.enums.TipoEscola;
+import exceptions.BancoDeDadosException;
 import interfaces.Service;
+import repository.EscolaRepository;
 
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class EscolaService implements Service<Escola> {
+public class EscolaService {
+    private EscolaRepository escolaRepository;
 
-    public Escola listarPorId(int id) {
-        for(Escola escola : escolas){
-            if (escola.getId() == id){
-                return escola;
-            }
-        } throw new NoSuchElementException("Escola com o id " + id + " não encontrada.");
+    public EscolaService(){
+        escolaRepository = new EscolaRepository();
+    }
+
+    public void listarPorId(int id) {
+        try {
+            List<Escola> listar = escolaRepository.listar();
+            listar.forEach(System.out::println);
+        } catch (BancoDeDadosException e) {
+            e.printStackTrace();
+        }
     }
 
     //CREATED
-    @Override
-    public boolean salvar(Escola escola) {
-        for (Escola escola1 : escolas) {
-            if (escola1.getNome().equals(escola.getNome())) {
-                System.out.println("A escola " + escola.getNome() + " já foi cadastrada!");
-                return false;
-            }
-
+    public void salvar(Escola escolaNova) {
+        try {
+            Escola escolaAdicionada = escolaRepository.adicionar(escolaNova);
+            System.out.println("Escola adicinada com sucesso! " + escolaAdicionada);
+        } catch (BancoDeDadosException e) {
+            System.out.println("ERRO: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e.getMessage());
+            e.printStackTrace();
         }
-        escola.setId(COUNTER.incrementAndGet());
-        escolas.add(escola);
-        System.out.println("Escola cadastrada com sucesso!\n");
-        return true;
     }
 
     //READ
-    @Override
     public void listarTodos(){
-        for (Escola escola : escolas){
-            System.out.println(escola.toString() + "\n");
+        try {
+            List<Escola> listar = escolaRepository.listar();
+            listar.forEach(System.out::println);
+        } catch (BancoDeDadosException e) {
+            e.printStackTrace();
         }
     }
 
 
 
     //UPDATED
-    @Override
-    public boolean atualizar(int id, Escola escola) {
-        for (Escola EscolaAtualizar : escolas) {
-            if (EscolaAtualizar.getId() == id) {
-                EscolaAtualizar.setNome(escola.getNome());
-                EscolaAtualizar.setTipo(escola.getTipo());
-                return true;
-            }
+    public void atualizar(int id, Escola escola) {
+        try {
+            boolean editado = escolaRepository.editar(id, escola);
+            System.out.println("Escola editada? " + editado + "| com id=" + id);
+        } catch (BancoDeDadosException e) {
+            e.printStackTrace();
         }
-        return false;
     }
-    //DELETED
-    @Override
-    public boolean deletar(Escola escola) {
-        return escolas.remove(escola);
+    //Remove
+    public void deletar(Integer id) {
+        try {
+            boolean deletado = escolaRepository.remover(id);
+            System.out.println("Escola deletada? " + deletado + "| com id=" + id);
+        } catch (BancoDeDadosException e) {
+            e.printStackTrace();
+        }
     }
 }
