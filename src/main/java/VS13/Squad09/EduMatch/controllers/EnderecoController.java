f7aa5d39ee66355aa25efd61949a6f3a6ef55efb
angelloantonnio <angelio123@hotmail.com>
2024-02-02 17:58:40 -0300

    refactor: adicao da classe component enderecoMapper e refatoracao das classes de endereco: entidade, repositorio e serviços para utilização do JPA

--- src/main/java/VS13/Squad09/EduMatch/controllers/EnderecoController.java
+++ src/main/java/VS13/Squad09/EduMatch/controllers/EnderecoController.java
@@ -4,10 +4,9 @@ package VS13.Squad09.EduMatch.controllers;
 import VS13.Squad09.EduMatch.controllers.interfaces.IEnderecoController;
 import VS13.Squad09.EduMatch.dtos.request.EnderecoCreateDTO;
 import VS13.Squad09.EduMatch.dtos.response.EnderecoDTO;
-import VS13.Squad09.EduMatch.exceptions.BancoDeDadosException;
-import VS13.Squad09.EduMatch.exceptions.NaoEncontradoException;
 import VS13.Squad09.EduMatch.services.EnderecoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -29,22 +28,23 @@ public class EnderecoController implements IEnderecoController {
 
     @PutMapping("/{idEndereco}")
     public ResponseEntity<EnderecoDTO> atualizar(@PathVariable("idEndereco") Integer idEndereco,
-                                                 EnderecoCreateDTO enderecoCreateDTO) throws BancoDeDadosException, NaoEncontradoException {
+                                                 @Valid @RequestBody EnderecoCreateDTO enderecoCreateDTO) throws Exception {
         return ResponseEntity.ok(enderecoService.atualizar(idEndereco, enderecoCreateDTO));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deletar(@PathVariable Integer id) throws BancoDeDadosException, NaoEncontradoException {
-        return ResponseEntity.ok(enderecoService.deletar(id));
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) throws Exception {
+        enderecoService.deletar(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/usuario/{id}")
-    public ResponseEntity<EnderecoDTO> listarPorDono(@PathVariable("id") Integer id) throws BancoDeDadosException, NaoEncontradoException {
-        return ResponseEntity.ok(enderecoService.listarPorDono(id));
+    public ResponseEntity<EnderecoDTO> listarPorDono(@PathVariable("id") Integer id) throws Exception {
+        return ResponseEntity.ok(enderecoService.findEnderecoByUsuarioId(id));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EnderecoDTO> listarPorId(@PathVariable("id") Integer id) throws BancoDeDadosException, NaoEncontradoException {
-        return ResponseEntity.ok(enderecoService.listarPorId(id));
+    public ResponseEntity<EnderecoDTO> listarPorId(@PathVariable("id") Integer id) throws Exception {
+        return ResponseEntity.ok(enderecoService.findByIdEndereco(id));
     }
 }
