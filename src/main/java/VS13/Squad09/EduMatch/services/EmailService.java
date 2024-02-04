c9cf7f9a29fb4030bdb4e6d9db9f19d391d29bb9
Vinícius Félix Santos De Jesus <vinicius.felix@dbccompany.com.br>
2024-02-04 14:55:03 -0300

    add: implementacao JPA certificado

--- src/main/java/VS13/Squad09/EduMatch/services/EmailService.java
+++ src/main/java/VS13/Squad09/EduMatch/services/EmailService.java
@@ -87,7 +87,7 @@ public class EmailService {
         Map<String, Object> dados = new HashMap<>();
         if (usuario != null){
             dados.put("nome", usuario.getNome());
-            dados.put("id", usuario.getId());
+            dados.put("id", usuario.getIdUsuario());
         }
         if (certificado != null){
             dados.put("nome", certificado.getUsuario().getNome());
