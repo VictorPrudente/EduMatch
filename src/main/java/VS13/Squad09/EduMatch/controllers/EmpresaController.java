2ffd33adfbf1b452168e5c3192e89c23bc730da5
Victor Prudente <143949574+VictorPrudente@users.noreply.github.com>
2024-02-10 16:51:53 -0300

    refact: EmpresaDTO e Tag Swagger

--- src/main/java/VS13/Squad09/EduMatch/controllers/EmpresaController.java
+++ src/main/java/VS13/Squad09/EduMatch/controllers/EmpresaController.java
@@ -3,6 +3,7 @@ package VS13.Squad09.EduMatch.controllers;
 import VS13.Squad09.EduMatch.controllers.interfaces.IEmpresaController;
 import VS13.Squad09.EduMatch.dtos.usuario.response.EmpresaDTO;
 import VS13.Squad09.EduMatch.services.UsuarioService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/parceiros")
+@Tag(name = "Empresas Parceiras")
 public class EmpresaController implements IEmpresaController {
 
     private final UsuarioService usuarioService;
